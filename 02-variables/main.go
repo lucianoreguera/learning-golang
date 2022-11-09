ce2291d35d0923b820334f21a750959e7aaf01db
lucianor <regueraluciano@gmail.com>
2022-11-09 02:28:54 -0300

    Variables

---
+++ 02-variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// V1 type static
+	// var name string = "Luciano"// default ""
+	// var age int     // default 0
+
+	// age = 41
+
+	// V2 type dynamic
+	name := "Luciano"
+	age := 41
+
+	fmt.Println(name)
+	fmt.Println(age)
+}
